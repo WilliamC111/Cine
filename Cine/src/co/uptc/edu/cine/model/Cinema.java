9c291b5d0154e0f3b080a0f7ada9edf9b245404a
WilliamC111 <110690003+WilliamC111@users.noreply.github.com>
2023-04-07 10:04:07 -0500

    Update Cinema.java

--- Cine/src/co/uptc/edu/cine/model/Cinema.java
+++ Cine/src/co/uptc/edu/cine/model/Cinema.java
@@ -1,37 +1,53 @@
 package co.uptc.edu.cine.model;
 
 public class Cinema {
-
-	public String cinema;
-	public int hour;
-
-	public int isTimeToMovie() {
-		int timeToMovie = 0;
-		return timeToMovie;
+public String cinema;
+	private int hour;
+	private ArrayList<Room> rooms;
+	private ArrayList<Movie> movies;
+	private Customer customer;
+	
+	public String isTimeToMovie() {
+		return cinema;
 	}
-
-	public void checkSeat() {
-
+	public boolean checkSeat () {
+		return false;
+		
 	}
-
 	public void cancelTicket() {
-
+		
 	}
-
 	public String getCinema() {
 		return cinema;
 	}
-
-	public void setCinema(String cinema) {
-		this.cinema = cinema;
-	}
-
 	public int getHour() {
 		return hour;
 	}
-
+	public void setCinema(String cinema) {
+		this.cinema = cinema;
+	}
+	
 	public void setHour(int hour) {
 		this.hour = hour;
 	}
+	public ArrayList<Room> getRooms() {
+		return rooms;
+	}
+	public void setRooms(ArrayList<Room> rooms) {
+		this.rooms = rooms;
+	}
+	public ArrayList<Movie> getMovies() {
+		return movies;
+	}
+	public void setMovies(ArrayList<Movie> movies) {
+		this.movies = movies;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
 
 }
