b750365c6d53ae888124d742382b34df558dba62
WilliamC111 <110690003+WilliamC111@users.noreply.github.com>
2023-04-07 10:07:07 -0500

    Update Room.java

--- Cine/src/co/uptc/edu/cine/model/Room.java
+++ Cine/src/co/uptc/edu/cine/model/Room.java
@@ -1,5 +1,30 @@
 package co.uptc.edu.cine.model;
 
+import java.util.ArrayList;
+
 public class Room {
+	private short roomNumber;
+	private ArrayList<Movie> movies;
+
+	public void addMovie(Movie movie) {
+
+	}
+
+	public short getRoomNumber() {
+		return roomNumber;
+	}
+
+	public ArrayList<Movie> getMovies() {
+		return movies;
+	}
+
+	public void setRoomNumber(short roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public void setMovies(ArrayList<Movie> movies) {
+		this.movies = movies;
+	}
 
 }
+
