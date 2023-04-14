436380f0f44e226aadf3ad1395289fd62c4eef77
WilliamC111 <110690003+WilliamC111@users.noreply.github.com>
2023-04-14 10:52:10 -0500

    Update Presenter.java

--- Cine/src/co/uptc/edu/cine/presenter/Presenter.java
+++ Cine/src/co/uptc/edu/cine/presenter/Presenter.java
@@ -1,5 +1,91 @@
 package co.uptc.edu.cine.presenter;
 
-public class Presenter {
+import co.uptc.edu.cine.model.Cinema;
+import co.uptc.edu.cine.model.Customer;
+import co.uptc.edu.cine.model.Movie;
+import co.uptc.edu.cine.model.MovieFormat;
+import co.uptc.edu.cine.model.MovieGender;
+import co.uptc.edu.cine.view.View;
 
+public class Presenter {
+	Cinema cinema = new Cinema();
+	View view = new View();
+
+	public Presenter() {
+		init();
+	}
+
+	public void init() {
+		String menu = "       .....BIENVENIDOS A CINE.....      " + "\n1 - Registar Cliente\n2 - Mostrar Cartelera"
+				+ "\n3 - Ver usuarios\n4 - Salir";
+		try {
+			int option = view.readGraphicInt(menu + "\nSeleccione una opción:");
+			switch (option) {
+			case 1:
+				createCustomer();
+				break;
+			case 2:
+				ShowBillboard();
+				break;
+			case 3:
+                ShowCustomers();
+				break;
+			
+			case 4:
+				view.showGraphicMessage("----Adios----");
+				System.exit(0);
+				break;
+			default:
+				view.showErrorMessage("OPCION NO VALIDA");
+				init();
+				break;
+			}
+		} catch (Exception e) {
+			view.showErrorMessage("OPCION NO VALIDA");
+			init();
+		}
+
+	}
+
+	public void createCustomer() {
+		String menu1 = "       ...Registrar cliente...      "
+				+ "\n1 - Ingrese Nombre\n2 - Ingrese identificacion del Cliente\n3 - Salir";
+
+		view.showGraphicMessage(menu1);
+		Customer customer = new Customer();
+		customer.setName(view.readGraphicString("Ingrese nombre del paciente:"));
+		customer.setID(view.readGraphicInt("Ingrese identificacion del cliente:"));
+		view.showGraphicMessage("CLIENTE CREADO");
+		init();
+	}
+
+	public void ShowBillboard() {
+		Movie movie = new Movie();
+		movie.setIcon("Cine/src/resources/El_Gato_Con_Botas.jpg");
+		System.out.println(movie);
+	}
+
+	public void ShowCustomers() {
+		Movie movie = new Movie();
+		movie.setNameMovies("El gato con botas");
+		movie.setDescription(
+				"""
+						El Gato con Botas descubre que su pasión por la aventura le ha pasado factura: Ha agotado ocho de sus nueve vidas.
+						El Gato con Botas se embarca en un viaje épico para encontrar al mítico Último Deseo y recuperar sus nueve vidas.
+						""");
+		movie.setTimeMovie("1h 42min");
+		movie.setMovieGenders(MovieGender.ADVENTURE);
+		movie.setMovieGenders(MovieGender.ANIMATION);
+		movie.setMovieGenders(MovieGender.COMEDY);
+		movie.setMovieFormat(MovieFormat.TWO_D);
+
+		movie.setIcon("Cine/src/resources/El_Gato_Con_Botas.jpg");
+
+		System.out.println(movie);
+	}
+
+	public static void main(String[] args) {
+		new Presenter();
+	}
 }
+
